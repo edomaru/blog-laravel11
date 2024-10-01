0ef9f13f0779313dae9391f9763fda3e5628f67e
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-10-01 13:40:43 +0700

    Add additional information to the blog posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,8 @@
     <title>Laravel Blog</title>
 </head>
 <body>
-    <h1>Post: {{ $post }}</h1>
+    <h1>{{ $post->title }}</h1>
+    <div>{{ $post->author }} / {{ $post->date->diffForHumans() }}</div>
+    <div>{{ $post->contents }}</div>
 </body>
 </html>
